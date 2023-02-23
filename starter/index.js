b2359cf55953bab28bf474b4e373db098b2ed9de
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2023-02-23 16:40:17 +0000

    added files to repo

---
+++ starter/index.js
@@ -0,0 +1,18 @@
+// ## psuedocode for task
+// Create a variable to store the current date and time using the Moment.js library
+
+// Display the current date and time at the top of the calendar
+
+// Create timeblocks for standard business hours (e.g. 9am-5pm) using a for loop
+
+// Within the for loop, create a color-coding system to distinguish between past, present, and future timeblocks
+
+// Allow a user to enter an event by clicking on a timeblock and display the input field within that timeblock
+
+// Create a save button within each timeblock and use an event listener to save the entered event to local storage when clicked
+
+// Use another event listener to retrieve the saved events from local storage and display them on the calendar upon page refresh
+
+// Ensure that the events are persisted between refreshes of a page
+
+// Follow best practices for file structure, naming conventions, indentation, and commenting within the jQuery JavaScript file.
